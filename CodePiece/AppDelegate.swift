f6fa19be41f0a832a5c3b33630da73134a29b002
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-19 21:26:01 +0900

    Initial Commit

---
+++ CodePiece/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/19.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ CodePiece/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/19.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
