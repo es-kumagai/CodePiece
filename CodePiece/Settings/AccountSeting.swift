a683e752720814475e23042134a1ab3344c56d50
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-20 00:16:50 +0900

    幾つかの言語候補を追加しました。

---
+++ CodePiece/Settings/AccountSeting.swift
@@ -0,0 +1,13 @@
+//
+//  AccountSeting.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/19.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Foundation
+
+struct AccountSetting {
+	
+}
---
+++ CodePiece/Settings/ProjectSetting.swift
@@ -0,0 +1,12 @@
+//
+//  ProjectSetting.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/19.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+struct ProjectSetting {
+
+	var hashTag:String
+}
