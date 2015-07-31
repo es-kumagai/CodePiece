e83df6333348c48bcb13685b9e82eae783b06278
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-08-01 02:26:34 +0900

    Add Icons.

--- CodePiece/BaseWindowController.swift
+++ CodePiece/BaseWindowController.swift
@@ -12,27 +12,6 @@ class BaseWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-
-		if let info = NSBundle.mainBundle().infoDictionary, let buildVersion = info["CFBundleVersion"] {
-			
-			if let window = self.window {
-				
-				self.window?.title = "\(window.title) (build: \(buildVersion))"
-			}
-		}
-
-		// FIXME: @koogawa さんの環境で何も表示されないらしいので、応急措置を講じてみました。
-		if self.contentViewController == nil {
-			
-			if let window = self.window {
-				
-				self.window?.title = "\(window.title) (Fix:#1)"
-			}
-
-			let controller = self.storyboard?.instantiateControllerWithIdentifier("MainViewController") as? NSViewController
-
-			self.contentViewController = controller
-		}
     }
 
 }
