a94142cd17e0ce753408c18552322883dd704e89
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-31 16:41:35 +0900

    Update Info.plist.

---
+++ CodePiece/Windows/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/31.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ CodePiece/Windows/AboutWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutWindowController.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/07/31.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class AboutWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
