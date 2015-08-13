ccc131390ee6037b1875cc63035cf5a28035e9f7
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-08-13 09:45:30 +0900

    Remove main.swift.

--- CodePiece/AppDelegate.swift
+++ CodePiece/AppDelegate.swift
@@ -14,6 +14,7 @@ import ESNotification
 var sns:SNSController!
 var captureController:WebCaptureController!
 
+@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, AlertDisplayable {
 
 	var urlSchemeManager:URLSchemeManager!
