744dfb71b64f6fe7b5ce1488258c4794d6a6154e
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-06 10:28:27 +0900

    Fixed problem which an error occurred when "display_url" is not found.

--- ESTwitterAPI/Data/URLInfo.swift
+++ ESTwitterAPI/Data/URLInfo.swift
@@ -10,10 +10,26 @@ import Himotoki
 
 public struct URLInfo {
 	
-	public var displayUrl:String
-	public var expandedUrl:URL
+	public var displayUrl:String?
+	public var expandedUrl:URL?
 	public var url:URL
 	public var indices:Indices
+}
+
+extension URLInfo {
+
+	public var effectiveUrl:URL {
+		
+		return self.expandedUrl ?? self.url
+	}
+}
+
+extension URLInfo : CustomStringConvertible {
+
+	public var description:String {
+	
+		return self.displayUrl ?? self.effectiveUrl.description
+	}
 }
 
 extension URLInfo : Decodable {
@@ -22,8 +38,8 @@ extension URLInfo : Decodable {
 		
 		return try build(URLInfo.init)(
 		
-			e <| "display_url",
-			e <| "expanded_url",
+			e <|? "display_url",
+			e <|? "expanded_url",
 			e <| "url",
 			e <| "indices"
 		)
