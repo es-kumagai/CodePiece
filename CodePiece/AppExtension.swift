d1301f90860768724fba0724dcf25c19af873d30
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-11 15:53:59 +0900

    CodePiece now can open Twitter & GitHub Home Page from icons on status bar of MainWindow.

--- CodePiece/AppExtension.swift
+++ CodePiece/AppExtension.swift
@@ -35,56 +35,5 @@ extension NSApplication {
 	func showPreferencesWindow() {
 		
 		preferencesWindowController.showWindow(self)
-	}
-	
-	func canOpenTwitterHome() -> Bool {
-
-		return sns?.twitter.credentialsVerified ?? false
-	}
-	
-	func openTwitterHome() {
-		
-		guard let username = sns.twitter.account?.username else {
-		
-			return self.showErrorAlert("Failed to open Twitter", message: "Twitter user is not set.")
-		}
-		
-		do {
-
-			try ESTwitter.Browser.openWithUsername(username)
-		}
-		catch ESTwitter.Browser.Error.OperationFailure(reason: let reason) {
-			
-			self.showErrorAlert("Failed to open Twitter", message: reason)
-		}
-		catch {
-			
-			self.showErrorAlert("Failed to open Twitter", message: "Unknown Error : \(error)")
-		}
-	}
-
-	func canOpenGitHubHome() -> Bool {
-		
-		return settings?.account.authorizationState.isValid ?? false
-	}
-	
-	func openGitHubHome() {
-		
-		guard let username = settings.account.username else {
-			
-			return self.showErrorAlert("Failed to open GitHub", message: "GitHub user is not set.")
-		}
-		
-		let urlString = "https://GitHub.com/\(username)"
-		
-		guard let url = NSURL(string: urlString) else {
-			
-			return self.showErrorAlert("Failed to open GitHub", message: "Invalid URL '\(urlString)'.")
-		}
-
-		NSWorkspace.sharedWorkspace().openURL(url).ifFalse {
-
-			self.showErrorAlert("Failed to open GitHub", message: "URL '\(url)' cannot open.")
-		}
-	}
+	}	
 }
