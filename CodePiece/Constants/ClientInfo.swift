adee69783fc7820421087b04907f1813e3414c55
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-10-12 19:36:55 +0900

    Fix definition of CodePieceClientInfo.

---
+++ CodePiece/Constants/ClientInfo.swift
@@ -0,0 +1,11 @@
+//
+//  ClientInfo.swift
+//  CodePiece
+//
+//  Created by Tomohiro Kumagai on H27/10/12.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+extension CodePieceClientInfo : GitHubClientInfoType {
+	
+}
