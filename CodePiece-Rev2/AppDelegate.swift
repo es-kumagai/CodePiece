9fe227dc7a6480458f87b5edfd95bb10af55b751
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-01-11 23:36:15 +0900

    Initial Commit

---
+++ CodePiece-Rev2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodePiece-Rev2
+//
+//  Created by Tomohiro Kumagai on 2020/01/11.
+//  Copyright © 2020 Tomohiro Kumagai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CodePiece-Rev2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodePiece-Rev2
+//
+//  Created by Tomohiro Kumagai on 2020/01/11.
+//  Copyright © 2020 Tomohiro Kumagai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
