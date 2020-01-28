f7c01000857c7fc6d1ddf6c39b56c576b2870666
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-01-28 09:47:09 +0900

    Have been fixed around a gist capture.

--- CodePiece/Codes/WebCaptureController.swift
+++ CodePiece/Codes/WebCaptureController.swift
@@ -76,8 +76,8 @@ extension WebCaptureController {
 			
 			super.init()
 			
-			self.view.navigationDelegate = self
-			self.view.customUserAgent = captureInfo.userAgent
+			view.navigationDelegate = self
+			view.customUserAgent = captureInfo.userAgent
 		}
 		
 		func post() {
@@ -116,36 +116,94 @@ extension WebCaptureController.Request : WKNavigationDelegate {
 		
 		DispatchQueue.main.async { [unowned self] in
 
+			let applyingStyleScript = """
+				const tableNode = document.getElementsByTagName('table')[0];
+				tableNode.style.tabSize = '4';
+				const containerNode = document.getElementById('file-codepiece-swift');
+				const searchNodes = containerNode.getElementsByTagName('div');
+
+				for (let i = 0; i != searchNodes.length; ++i) {
+					const node = searchNodes[i];
+					if (node.getAttribute('itemprop') == 'text') {
+						node.style.tabSize = '4';
+						node.style.border = 'thin solid #f7f7f7';
+						node.style.padding = '6px';
+						node.style.width = '\(self.captureInfo.clientSize.width)px';
+						node.style.maxHeight = '\(self.captureInfo.clientSize.height)px';
+						node.style.overflow = 'auto';
+						break;
+					}
+				}
+				"""
+			
+			let gettingBoundsScript = """
+				const targetNode = document.getElementsByClassName('data')[0];
+				const bounds = targetNode.getBoundingClientRect();
+
+				const x = bounds.left;
+				const y = bounds.top;
+				const width = bounds.width;
+				const height = bounds.height;
+
+				const bodyWidth = document.body.offsetWidth;
+				const bodyHeight = document.body.offsetHeight;
+
+				[x, y, width, height, bodyWidth, bodyHeight];
+				"""
+			
+			webView.evaluateJavaScript("\(applyingStyleScript)\n\(gettingBoundsScript)") { object, error in
+
+				guard let object = object as? Array<Int> else {
+					
+					return self.fulfillRequest(for: nil)
+				}
+				
+				let x = object[0]
+				let y = object[1]
+				let width = object[2]
+				let height = object[3]
+				let bodyWidth = object[4]
+				let bodyHeight = object[5]
+				
+				let rect = NSRect(x: x, y: y, width: width, height: height)
+				
+				self.view.frame = NSRect(x: 0, y: 0, width: bodyWidth, height: bodyHeight)
+				
+				print(rect)
+				
+				let configuration = instanceApplyingExpression(with: WKSnapshotConfiguration()) { settings in
+					
+					settings.rect = rect
+				}
+				
+				webView.takeSnapshot(with: configuration) { image, error in
+					
+					guard let image = image else {
+						
+						self.fulfillRequest(for: nil)
+						return
+					}
+					
+					self.fulfillRequest(for: image)
+				}
+			}
 //			// TODO: 汎用性を確保するためには DOM を渡して切り取る範囲を返す機能を切り出す必要があります。
-//			let dom = frame.domDocument!
+//			let content = frame!.domDocument!.getElementsByClassName("blob-file-content")!.item(0)
+//			let contentBound = node?.boundingBox()
+			
 //			let content = self.captureInfo.targetNode(sender, dom)
 //
 //			// TODO: content が取得できず nil になる場合もあるので対応が必要
 //			if let contentBound = content?.boundingBox() {
 //
-//				let image = frame.frameView.documentView!.capture(contentBound)
+////				let image = frame.frameView.documentView!.capture(contentBound)
 //
-//				self.fulfillRequest(image)
 //			}
 //			else {
 //
 //				self.fulfillRequest(for: nil)
 //			}
 			
-			let configuration = instanceApplyingExpression(with: WKSnapshotConfiguration()) { settings in
-				
-			}
-			
-			webView.takeSnapshot(with: configuration) { image, error in
-
-				guard let image = image else {
-					
-					self.fulfillRequest(for: nil)
-					return
-				}
-				
-				self.fulfillRequest(for: image)
-			}
 		}
 	}
 	
