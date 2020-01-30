9d47ada9bfbfde3d5896541bdf4a02d1a409b716
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-01-31 01:38:32 +0900

    Replace Binary Framework.

--- EncryptionTool/main.swift
+++ EncryptionTool/main.swift
@@ -8,14 +8,31 @@
 
 import Foundation
 import Ocean
+import CodePieceSupport
 
 let text = ProcessInfo.processInfo.arguments[1]
 
 do {
 
-	let data = try aes.encrypto(text, initialVector: initialVector)
+	let data = try cipher.encrypto(text, initialVector: nil)
 	
+	print("Source: \(text)")
+	print()
 	
+	print("Array<Int>:")
+	print("[", data.map { String(format: "%02X", $0) }.joined(separator: ", "), "]")
+	print()
+	
+	print("Plist Display Style:")
+	print("{length = \(data.count), 0x\(data.map { String(format: "%02X", $0) }.joined())}")
+	print()
+	
+	print("Base64 Encoding:")
+	print(data.base64EncodedString())
+	print()
+	
+	print("Decrypted String:")
+	print(try! cipher.decrypto(data, initialVector: nil))
 }
 catch {
 
