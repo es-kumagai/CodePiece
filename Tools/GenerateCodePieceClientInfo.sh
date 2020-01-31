edb4ec3c9a2ef721a226663b2533d0c4b8eb2187
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-02-01 04:57:09 +0900

    CodePiece 1 is now unavailable.

--- Tools/GenerateCodePieceClientInfo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CLIENTINFO="CodePieceClientInfo"
-CLIENTINFO_FILE="${CLIENTINFO}.plist"
-CLIENTINFO_DESTINATION_DIR="/private/tmp"
-
-echo "Generating '${CLIENTINFO_FILE}' ..."
-/usr/local/bin/XcodeGenerateConstants "${CLIENTINFO}" "${CLIENTINFO_DESTINATION_DIR}"
